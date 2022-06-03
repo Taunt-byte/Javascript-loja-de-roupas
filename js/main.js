68e779963e2fca76c4ba17bb3b21553bffbf445e
Lucas Neves <71947586+Taunt-byte@users.noreply.github.com>
2022-06-03 14:28:33 -0300

    Update dia 3

--- js/main.js
+++ js/main.js
@@ -4,11 +4,11 @@ const paragraph = document.querySelector('p');
 button.addEventListener('click', updateButton);
 
 function updateButton() {
-  if (button.value === 'Comprar') {
+  if (button.value === 'Calca') {
     button.value = 'Vendido';
     paragraph.textContent = '2 produtos disponiveis';
   } else {
-    button.value = 'Comprar';
+    button.value = 'Calca';
     paragraph.textContent = '3 produtos disponiveis.';
   }
 }
